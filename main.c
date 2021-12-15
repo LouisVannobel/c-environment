a9634217d4c830ccd5f988db7d82c39442fea727
Joela Rasamimanana <rasamimananajoela@gmail.com>
2021-12-15 10:06:19 +0100

    code guessing sans erreur

--- main.c
+++ main.c
@@ -6,14 +6,14 @@ int main(int argc,char**argv){
 	srand (time(NULL));
 	int number_to_guess=rand() %100+1;
 	int guess;
-	printf("I have in mind a number in between 1 and 100, can ypi find it?\n");
+	printf("I have in mind a number in between 1 and 100, can you find it?\n");
 	while(guess != number_to_guess){
 		scanf("%d",&guess);
 		if(guess<number_to_guess && guess<1){
 			printf("the number to guess is higher\n");
 		}
 		else if( guess> number_to_guess && guess>100){
-			printf("the numberto guess is lower\n");
+			printf("the number to guess is lower\n");
 		}
 		else{
 			printf("error syntax")
@@ -23,7 +23,4 @@ int main(int argc,char**argv){
 	printf("You just found the number, it was indeed %d\n");
 	return 0;
 }
-
-	
-		
 	
