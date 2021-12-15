e2d8930107bfbccf8d277027990d2dafe4ffac9f
Joela Rasamimanana <rasamimananajoela@gmail.com>
2021-12-15 10:54:50 +0100

    guessing_game2

--- main.c
+++ main.c
@@ -3,24 +3,29 @@
 #include<time.h>
 
 int main(int argc,char**argv){
-	srand (time(NULL));
-	int number_to_guess=rand() %100+1;
-	int guess;
-	printf("I have in mind a number in between 1 and 100, can you find it?\n");
-	while(guess != number_to_guess){
+	if(argc==1){
+		srand (time(NULL));
+		int number_to_guess=rand() %100+1;
+		printf("I have in mind a number in between 1 and 100, can you find it?\n");
+		int guess;
 		scanf("%d",&guess);
-		if(guess<number_to_guess && guess<1){
-			printf("the number to guess is higher\n");
-		}
-		else if( guess> number_to_guess && guess>100){
-			printf("the number to guess is lower\n");
-		}
-		else{
-			printf("error syntax")
-			return 1;
-		
+		while(guess != number_to_guess){
+			if(guess>0 && guess<=100){
+				if(guess<number_to_guess){
+					printf("the number to guess is higher\n");
+					scanf("xd", &guess);
+				}
+				else if( guess> number_to_guess ){
+					printf("the number to guess is lower\n");
+					scanf("%d", &guess);
+				}
+			}
+			else{
+				printf("error syntax\n");
+				return 1;
+			}
+		printf("You just found the number, it was indeed %d\n",guess);
+		return 0;
 	}
-	printf("You just found the number, it was indeed %d\n");
-	return 0;
 }
-	
+}
