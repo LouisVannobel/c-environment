82048b5a9dae9298540a4c72bd446fece6cb4291
Joela Rasamimanana <rasamimananajoela@gmail.com>
2021-12-15 09:32:07 +0100

    code guessing game

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
-int main(){
-	for (int i = 0; i < 10; i++){
-		printf("%d\n", i);
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(int argc,char**argv){
+	srand (time(NULL));
+	int number_to_guess=rand() %100+1;
+	int guess;
+	printf("I have in mind a number in between 1 and 100, can ypi find it?\n");
+	while(guess != number_to_guess){
+		scanf("%d",&guess);
+		if(guess<number_to_guess && guess<1){
+			printf("the number to guess is higher\n");
+		}
+		else if( guess> number_to_guess && guess>100){
+			printf("the numberto guess is lower\n");
+		}
+		else{
+			printf("error syntax")
+			return 1;
+		
 	}
-return 0;
+	printf("You just found the number, it was indeed %d\n");
+	return 0;
 }
+
+	
+		
+	
